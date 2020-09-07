ce9cfabbdfee484d9c0dce157cfb1bf3d089d1c1
Alexey Pestov <alexey.pestov@x5.ru>
2020-09-07 09:27:15 +0300

    rework 9

--- Task2/src/T9.java
+++ Task2/src/T9.java
@@ -1,14 +1,11 @@
 public class T9 {
     public static void main(String[] args) {
-        Integer[] arrayInt = new Integer[]{15, 10, 51, -6, -5, 3, -10, -34, 0, 32, 56, -12, 24, -52};
-        for (int i = arrayInt.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (arrayInt[j] < arrayInt[j + 1]) {
-                    int tmp = arrayInt[j];
-                    arrayInt[j] = arrayInt[j + 1];
-                    arrayInt[j + 1] = tmp;
-                }
-            }
+        int[] arrayInt = new int[]{15, 10, 51, -6, -5, 3, -10, -34, 0, 32, 56, -12, 24, -52};
+        for (int i = 0; i <= (arrayInt.length-1)/2; i++)    {
+            int temp = arrayInt[i];
+            int arrayEnd = arrayInt.length - 1 - i;
+            arrayInt[i] = arrayInt[arrayEnd];
+            arrayInt[arrayEnd] = temp;
         }
         for (Integer el : arrayInt) {
             System.out.println(el);
